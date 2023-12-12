f0ca6db27b8176e3b046ae9d0e4ff7e24b29eb6a
Ethan Cavill <ethancavill@gmail.com>
2023-12-11 18:35:21 -0800

    Initial urql setup

--- frontend/src/app/(main)/dashboard/page.tsx
+++ frontend/src/app/(main)/dashboard/page.tsx
@@ -1,6 +1,33 @@
+'use client';
+
 import React from 'react';
+import { useAtom } from 'jotai';
+import { useQuery } from 'urql';
+
+import { userAtom } from '@/app/store/atoms';
 
 const Dashboard = () => {
+  const [user] = useAtom(userAtom);
+  const [result] = useQuery({
+    query: `
+      query GetUser($id: ID!) {
+        getUser(id: $id) {
+          id
+          username
+          email
+        }
+      }
+    `,
+    variables: { id: user.user?.id },
+  });
+
+  const { data, fetching, error } = result;
+
+  console.log(data);
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
   return <div className="flex h-screen flex-1 flex-col justify-center px-6 py-12 text-white lg:px-8">Hello</div>;
 };
 
--- frontend/src/app/context/providers.tsx
+++ frontend/src/app/context/providers.tsx
@@ -2,13 +2,17 @@
 
 import { createStore, Provider } from 'jotai';
 import ToastProvider from './toasts/toast-provider';
+import { Provider as AltProvider } from 'urql';
+import urqlClient from '@libs/urql';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const myStore = createStore();
 
   return (
-    <Provider store={myStore}>
-      <ToastProvider>{children}</ToastProvider>
-    </Provider>
+    <AltProvider value={urqlClient}>
+      <Provider store={myStore}>
+        <ToastProvider>{children}</ToastProvider>
+      </Provider>
+    </AltProvider>
   );
 }
