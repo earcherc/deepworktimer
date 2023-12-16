357d169c8c93295ca2c967ee42aa0496f9f7f2aa
Ethan Cavill <ethancavill@gmail.com>
2023-12-16 13:39:09 -0800

    Modal fixes/improvements

--- frontend/src/app/components/modal/modal.tsx
+++ frontend/src/app/components/modal/modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { CheckIcon, ExclamationTriangleIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import { useModalContext } from '@/app/context/modal/modal-context';
@@ -12,28 +12,32 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
     success: {
       icon: <CheckIcon className="h-6 w-6 text-green-600" />,
       bgColor: 'bg-green-100',
+      buttonColor: 'bg-green-600 hover:bg-green-500',
     },
     danger: {
       icon: <XCircleIcon className="h-6 w-6 text-red-600" />,
       bgColor: 'bg-red-100',
+      buttonColor: 'bg-red-600 hover:bg-red-500',
     },
     warning: {
       icon: <ExclamationTriangleIcon className="h-6 w-6 text-yellow-600" />,
       bgColor: 'bg-yellow-100',
+      buttonColor: 'bg-yellow-600 hover:bg-yellow-500',
     },
     default: {
       icon: null,
       bgColor: 'bg-gray-100',
+      buttonColor: 'bg-indigo-600 hover:bg-indigo-500',
     },
   };
 
-  const { icon, bgColor } = modalConfig
+  const { icon, bgColor, buttonColor } = modalConfig
     ? modalTypeClasses[modalConfig.type] || modalTypeClasses.default
     : modalTypeClasses.default;
 
   return (
     <Transition.Root show={!!modalConfig} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={hideModal}>
+      <Dialog as="div" className="relative z-30" onClose={hideModal}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -58,27 +62,33 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                <div className={`mx-auto flex h-12 w-12 items-center justify-center rounded-full ${bgColor}`}>
-                  {icon}
-                </div>
+                {icon && (
+                  <div className={`mx-auto flex h-12 w-12 items-center justify-center rounded-full ${bgColor}`}>
+                    {icon}
+                  </div>
+                )}
                 <div className="mt-3 text-center sm:mt-5">
                   <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                     {modalConfig?.title}
                   </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">{modalConfig?.message}</p>
-                  </div>
+                  {modalConfig?.message && (
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">{modalConfig.message}</p>
+                    </div>
+                  )}
                   {children}
                 </div>
                 {modalConfig && modalConfig.buttons && (
-                  <div className="mt-5 sm:mt-6">
+                  <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                     {modalConfig.buttons.map((button, index) => (
                       <button
                         key={index}
                         onClick={button.onClick}
-                        className={`inline-flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:mt-0 sm:w-auto sm:text-sm ${
-                          button.isPrimary ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-gray-300 hover:bg-gray-400'
-                        }`}
+                        className={`inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold ${
+                          button.isPrimary
+                            ? `${buttonColor} text-white`
+                            : 'bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+                        } sm:col-start-${button.isPrimary ? '2' : '1'}`}
                       >
                         {button.text}
                       </button>
