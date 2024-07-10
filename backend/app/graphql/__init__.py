67344da87cfd5537d14b527b7e40d332869658fe
Ethan Cavill <ethancavill@gmail.com>
2024-07-10 21:20:35 +0000

    Delete graphQL backend code

--- backend/app/graphql/__init__.py
+++
@@ -1,5 +0,0 @@
-from .queries import Query
-from .mutations.daily_goal import DailyGoalMutations
-from .mutations.study_block import StudyBlockMutations
-from .mutations.study_category import StudyCategoryMutations
-from .mutations.user import UserMutations
