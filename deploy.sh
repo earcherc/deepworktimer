bc309e2e681d22b319bce19f6f3b92e3071b6792
Ethan Cavill <ethancavill@gmail.com>
2024-07-17 13:00:47 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Pull the latest changes from Git
-git pull origin master  # Replace 'main' with your branch name if different
+git pull origin master
 
-# Rebuild and restart Docker containers
-docker-compose down
-docker-compose build
-docker-compose up -d
+docker compose down
+docker compose build
+docker compose up -d
 
-# Optional: Remove unused Docker images to free up space
 docker image prune -f
