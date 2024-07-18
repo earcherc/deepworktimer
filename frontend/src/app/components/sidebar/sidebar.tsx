262b6beb88cdadbd094bcbde25f8b57f98be1311
Ethan Cavill <ethancavill@gmail.com>
2024-07-18 22:41:34 +0000

    Layout changes

--- frontend/src/app/components/sidebar/sidebar.tsx
+++ frontend/src/app/components/sidebar/sidebar.tsx
@@ -1,12 +1,16 @@
 import StudyCategoryComponent from '../study-category/study-category';
 import DailyGoalComponent from '../daily-goal/daily-goal';
+import Timer from '../timer/timer';
 
 export default function Sidebar() {
   return (
-    <div className="h-100 flex w-72 flex-col ">
+    <div className="h-100 flex w-1/3 flex-col ">
       <div className="flex grow flex-col overflow-y-auto overflow-x-hidden">
         <nav className="flex flex-1 flex-col">
-          <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <ul role="list" className="flex flex-1 flex-col gap-y-4">
+            <li>
+              <Timer />
+            </li>
             <li>
               <DailyGoalComponent />
             </li>
