76024bf6499971a0dd5860982046a7b3e88a5eaa
Ethan Cavill <ethancavill@gmail.com>
2024-07-18 19:13:30 -0700

    Improve deploy script cleanup

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,13 @@ git pull origin master
 export NEXT_PUBLIC_COMMIT_HASH=$(git rev-parse HEAD)
 
 docker compose down
-docker compose build
-docker compose up -d
 
-docker image prune -af
-docker builder prune -af
+docker container prune -f
+docker network prune -f
+docker image prune -f
+
+docker compose build --pull
+docker compose up -d
+docker volume prune -f
+docker builder prune -f --filter until=24h
+docker image prune -a --force --filter "until=24h"
