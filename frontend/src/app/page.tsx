00c7985b5d0f4ba95004ff26019962c6b87ea162
Ethan Cavill <ethancavill@gmail.com>
2024-07-19 15:44:31 +0000

    Imrpove hero section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,9 @@
 
 import {
   ArrowPathIcon,
-  ChevronRightIcon,
   CloudArrowUpIcon,
   LockClosedIcon,
-  ServerIcon,
+  ServerIcon
 } from '@heroicons/react/20/solid';
 import classNames from 'classnames';
 import Image from 'next/image';
@@ -269,43 +268,25 @@ export default function Home() {
                 <path d="M100 200V.5M.5 .5H200" fill="none" />
               </pattern>
             </defs>
-            <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-              <path
-                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                strokeWidth={0}
-              />
-            </svg>
             <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
           </svg>
           <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
             <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-              <div className="flex">
-                <div className="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  <span className="font-semibold text-indigo-600">We’re hiring</span>
-                  <span className="h-4 w-px bg-gray-900/10" aria-hidden="true" />
-                  <a href="#" className="flex items-center gap-x-1">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    See open positions
-                    <ChevronRightIcon className="-mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  </a>
-                </div>
-              </div>
-              <h1 className="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                A better way to ship your projects
+              <h1 className="mt-10 max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                Focus. Create. Achieve.
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Esse id magna consectetur fugiat non dolor in ad laboris magna laborum ea consequat. Nisi irure aliquip
-                nisi adipisicing veniam voluptate id. In veniam incididunt ex veniam adipisicing sit.
+                Great work requires deep concentration. Our timer creates the structure you need to dive into your most challenging tasks. Minimize interruptions, maximize output, and unlock your full creative potential.
               </p>
               <div className="mt-10 flex items-center gap-x-6">
                 <a
-                  href="#"
+                  href="/register"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get started
+                  Create your space
                 </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                  Learn more <span aria-hidden="true">→</span>
+                <a href="/blog" className="text-sm font-semibold leading-6 text-gray-900">
+                  Read our blogs <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
