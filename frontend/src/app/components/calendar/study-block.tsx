fbf8d17804d4155bba2b51a6201ec19f9fee7317
Ethan Cavill <ethancavill@gmail.com>
2024-07-19 16:46:36 +0000

    Study group dark mode

--- frontend/src/app/components/calendar/study-block.tsx
+++ frontend/src/app/components/calendar/study-block.tsx
@@ -38,8 +38,8 @@ const StudyBlockComponent: React.FC<StudyBlockProps> = ({ block, calculatePositi
       className={classNames(
         'absolute left-0 right-0 flex items-center overflow-hidden rounded-lg text-xs leading-5 cursor-pointer mx-1',
         {
-          'bg-blue-50 hover:bg-blue-100 opacity-90': !isInProgress,
-          'bg-blue-100 hover:bg-blue-200': isInProgress,
+          'bg-blue-50 dark:bg-blue-900 hover:bg-blue-100 dark:hover:bg-blue-800 opacity-90': !isInProgress,
+          'bg-blue-100 dark:bg-blue-800 hover:bg-blue-200 dark:hover:bg-blue-700': isInProgress,
           'z-10': isInProgress,
         },
       )}
@@ -49,14 +49,14 @@ const StudyBlockComponent: React.FC<StudyBlockProps> = ({ block, calculatePositi
       }}
       onClick={onClick}
     >
-      <div className="flex-1 px-1 truncate text-blue-700 flex justify-between items-center">
+      <div className="flex-1 px-3 truncate text-blue-700 dark:text-blue-300 flex justify-between items-center">
         <div className="flex-shrink-0">
           <span className="font-semibold">{formatTime(startTime)}</span>
           {block.end_time && <span className="font-semibold"> - {formatTime(endTime)}</span>}
           {isInProgress && <span className=""> - In Progress</span>}
-          {isInProgress && <span className="ml-1 animate-pulse text-red-500">•</span>}
+          {isInProgress && <span className="ml-1 animate-pulse text-red-500 dark:text-red-400">•</span>}
         </div>
-        <span className="flex-shrink-0 text-blue-300">{block.is_countdown ? 'timer' : 'open'}</span>
+        <span className="flex-shrink-0 text-blue-300 dark:text-blue-500">{block.is_countdown ? 'timer' : 'open'}</span>
       </div>
     </li>
   );
