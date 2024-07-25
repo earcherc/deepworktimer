ca968c7e6c91254eb9ba8823457f902c876f8153
Ethan Cavill <ethancavill@gmail.com>
2024-07-25 23:35:20 +0000

    Update landing image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -179,7 +179,7 @@ export default function Home() {
                 </p>
               </div>
               <Image
-                src="/images/deepworktimer3.png"
+                src="/images/deepworktimer4.png"
                 alt="Deep Work Timer App screenshot"
                 className="relative -z-20 min-w-full max-w-xl rounded-xl shadow-xl ring-1 ring-white/10 lg:row-span-4 lg:w-[64rem] lg:max-w-none"
                 width={2432}
