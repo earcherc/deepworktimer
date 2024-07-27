3ff6dceb032e3b405857a58541dc2f5c15d64678
Ethan Cavill <ethancavill@gmail.com>
2024-07-27 14:38:10 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
 git pull origin master
-
 export NEXT_PUBLIC_COMMIT_HASH=$(git rev-parse HEAD)
 
 docker compose down
 
-docker container prune -f
-docker network prune -f
-docker image prune -f
+docker system prune --force
+
+docker image prune --all --force --filter "until=24h"
+docker builder prune --all --force --filter "until=24h"
+
+docker volume ls -q | grep -v "deepworktimer_db_data\|deepworktimer_redis_data" | xargs -r docker volume rm
 
 docker compose build --pull
 docker compose up -d
-docker volume prune -f
-docker builder prune -f --filter until=24h
-docker image prune -a --force --filter "until=24h"
+
+docker system df
