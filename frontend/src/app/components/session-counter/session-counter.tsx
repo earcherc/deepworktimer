8fc0bbf3090e6cae7ed92d7ef4f618b9574c908f
Ethan <ethancavill@gmail.com>
2024-10-23 15:51:48 -0700

    Cleaner seesion counter

--- frontend/src/app/components/session-counter/session-counter.tsx
+++ frontend/src/app/components/session-counter/session-counter.tsx
@@ -11,37 +11,24 @@ interface SessionCounterProps {
 }
 
 const SessionCounter: FC<SessionCounterProps> = ({ target, completed, isActive, onReset, onClick }) => {
-  const renderDots = () => {
-    const dots = [];
-
-    for (let i = 0; i < target; i++) {
-      const isHalfFilled = i === completed && isActive;
-
-      dots.push(
-        <div
-          key={i}
-          className={classNames('w-4 h-4 rounded-full mb-1', {
-            'bg-gray-300': i >= completed && !isHalfFilled,
-            'bg-blue-500': i < completed,
-            'relative overflow-hidden bg-gray-300': isHalfFilled
-          })}
-        >
-          {isHalfFilled && (
-            <div 
-              className="absolute inset-0 bg-blue-500" 
-              style={{ clipPath: 'inset(0 50% 0 0)' }} 
-            />
-          )}
-        </div>
-      );
-    }
-
-    return dots;
-  };
-
   return (
     <div className="flex mt-2 mb-3 cursor-pointer relative group max-w-48" onClick={onClick}>
-      <div className="flex space-x-4 transition-colors flex-wrap">{renderDots()}</div>
+      <div className="flex space-x-4 transition-colors flex-wrap">
+        {[...Array(target)].map((val, i) => (
+          <div
+            key={val + i}
+            className={classNames('w-4 h-4 rounded-full mb-1', {
+              'bg-gray-300': i >= completed && !(i === completed && isActive),
+              'bg-blue-500': i < completed,
+              'relative overflow-hidden bg-gray-300': i === completed && isActive,
+            })}
+          >
+            {i === completed && isActive && (
+              <div className="absolute inset-0 bg-blue-500" style={{ clipPath: 'inset(0 50% 0 0)' }} />
+            )}
+          </div>
+        ))}
+      </div>
       {completed > 0 && (
         <button
           className="absolute -mt-px -right-10 top-1/2 transform -translate-y-1/2 text-red-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gray-50 rounded-full p-1"
