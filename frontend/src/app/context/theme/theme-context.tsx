3257bc5ec8647ee74f30ed8dd31413104232b085
Ethan <ethancavill@gmail.com>
2024-10-24 15:51:13 -0700

    Change default to light mode

--- frontend/src/app/context/theme/theme-context.tsx
+++ frontend/src/app/context/theme/theme-context.tsx
@@ -12,7 +12,7 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [theme, setTheme] = useState<Theme>('dark');
+  const [theme, setTheme] = useState<Theme>('light');
 
   useEffect(() => {
     const storedTheme = localStorage.getItem('theme') as Theme | null;
